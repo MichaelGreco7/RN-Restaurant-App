4731cd9f18b918048f2f25988e2cfb6b87380851
Michael Greco <michael.greco07@gmail.com>
2020-04-17 08:11:18 -0400

    configuring axios

---
+++ src/api/yelp.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://api.yelp.com/v3/buisnesses',
+  headers: {
+    Authorization:
+      'Bearer yNeQMNZA1hb1J7jUPH1j1EKpvCVdwsCFp3BmtDwKX61Kycq3dbQ045QGUWbTRVSyciNSyZdAM0oZSv8sg1nBjiHlIIZClM0ZYYJL_Jo8OT5APm5IO5C74fzlcXaZXnYx',
+  },
+});
