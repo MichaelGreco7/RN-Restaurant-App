1b640efe360414b6d59dc93c9b4cd3c01fb31d61
Michael Greco <michael.greco07@gmail.com>
2020-04-17 11:48:56 -0400

    grouping results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
-const ResultsList = ({ title }) => {
+const ResultsList = ({ title, results }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
+      <Text>Results: {results.length} </Text>
     </View>
   );
 };
